0368d7fdc77b3ea4c1aac24f6c8c5046ca60c191
Yarmak Yaroslav <yarmayaroslav@gmail.com>
2024-09-26 13:34:29 +0300

    Add files via upload

--- lab.sql
+++ lab.sql
@@ -1,1 +1,18 @@
-select * from cities;
+SELECT * FROM `cities` LIMIT 50
+SELECT *
+FROM cities
+ORDER BY population ASC
+LIMIT 10
+OFFSET 10;
+SELECT *
+FROM cities
+ORDER BY name DESC
+LIMIT 30;
+SELECT *
+FROM cities
+ORDER BY region ASC, population DESC;
+SELECT DISTINCT region
+FROM cities;
+SELECT *
+FROM cities
+ORDER BY region DESC, name DESC;
